b7dbec9c22e0f65e5ffaf1f412bdaa679a2f6131
Lucian <luciandc5@gmail.com>
2023-12-08 13:55:21 +0100

    Style Adaptions
    - Plots with frame
    - Bit Error Rate -> BER
    - title of figure contains "channel" instead of label of axes

--- Signal/constellation.py
+++ Signal/constellation.py
@@ -12,8 +12,7 @@ class Constellation:
         self.constellation_map = np.array([np.binary_repr(i, width=int(np.log2(modulation))) for i in range(modulation)])
 
 
-    def plot(self, ax: plt.Axes = None):
-        flag = False
+    def plot(self, ax: plt.Axes = None, flag=0):
         # Plotting of N-PSK constellation
         if ax is None:
             fig, ax = plt.subplots(figsize=(5, 5))
@@ -21,12 +20,19 @@ class Constellation:
         ax.plot(np.real(self.constellation), np.imag(self.constellation), 'k*')
         for _i in range(len(self.constellation_map)):
             ax.text(np.real(self.constellation[_i]), np.imag(self.constellation[_i]), self.constellation_map[_i])
-        if flag:
+        if flag == 1:
             ax.grid()
             ax.legend(['8-PSK constellation'])
             ax.set_xlabel('In-phase Component')
             ax.set_ylabel('Quadrature Component')
             ax.set_title(f'Constellation Plot for {self.modulation}-PSK')
+        elif flag == 2:
+            ax.set_xlim([-2, 2])
+            ax.set_ylim([-2, 2])
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.axvline(0, color='k', linestyle='--')
+            ax.axhline(0, color='k', linestyle='--')
         else:
             ax.set_xlim([-2, 2])
             ax.set_ylim([-2, 2])
--- main.py
+++ main.py
@@ -26,9 +26,10 @@ for modulation in [2, 4, 8, 16]:
     # Simulate AWGN channel with variable SNR
     channel_classes = [AWGNChannel, SingleRayleighChannel, EqualizedSingleRayleighChannel]
     fig, axs = plt.subplots(len(all_snr) + 1, len(channel_classes), figsize=(7, 12), sharex=False, sharey="row")
-    axs[0, 0].set_title("AWGN Channel")
-    axs[0, 1].set_title("Unequalized Single Rayleigh Channel")
-    axs[0, 2].set_title("Equalized Single Rayleigh Channel")
+    fig.suptitle(f'Channels {modulation}-PSK constellation\n', size=13 )
+    axs[0, 0].set_title("AWGN", fontsize=10)
+    axs[0, 1].set_title("Unequalized Single Rayleigh", fontsize=10)
+    axs[0, 2].set_title("Equalized Single Rayleigh", fontsize=10)
     bit_error_rate = np.zeros((len(all_snr), len(channel_classes)))
     for j, channel_class in enumerate(channel_classes):
         # bit_error_rate = np.zeros(len(all_snr))
@@ -41,12 +42,12 @@ for modulation in [2, 4, 8, 16]:
             color = ["tab:green" if bit else "tab:red" for bit in recieved_signal.symbols == signal.symbols]
             min_distance = _pairwise_distance(rx_symbols, const.constellation).min(axis=1)
             ax.scatter(rx_symbols.real, rx_symbols.imag, color=color, alpha=0.5)
-            ax.set_ylabel(f"SNR = {snr} dB")
-            const.plot(ax=axs[i, j])
+            ax.set_ylabel(f"SNR {snr}dB", fontsize=8)
+            const.plot(ax=axs[i, j], flag = 2)
         axs[-1, j].plot(all_snr, bit_error_rate[:,j], color="tab:blue")
         axs[-1, j].plot(all_snr, bit_error_rate[:,j], 'x', color="tab:orange")    
         axs[-1, j].set_xlabel("SNR (dB)")
-        axs[-1, j].set_ylabel("Bit Error Rate")
+        axs[-1, j].set_ylabel("BER")
         axs[-1, j].spines[["right", "top"]].set_visible(False)
 
     bit_error_rate_for_all_constellations[const.modulation] = bit_error_rate
@@ -59,10 +60,11 @@ styles = {2: '-D', 4: '--o', 8: ':s', 16: '-^'}
 for key, value in bit_error_rate_for_all_constellations.items():
     color = ""
     plt.plot(all_snr, value, styles[key])
-plt.yscale('log')
-plt.xlabel("SNR (dB)")
-plt.ylabel("Bit Error Rate")
-plt.title(f"Bit Error Rate vs SNR for {key}-PSK")
-plt.legend(["AWGN", "Single Rayleigh", "Equalized Single Rayleigh"])
-plt.tight_layout()
+    plt.yscale('log')
+    plt.xlabel("SNR (dB)")
+    plt.ylabel("Bit Error Rate")
+    plt.title(f"Bit Error Rate vs SNR for {key}-PSK")
+    plt.legend(["AWGN", "Single Rayleigh", "Equalized Single Rayleigh"])
+    plt.tight_layout()
+    plt.show()
 # %%
